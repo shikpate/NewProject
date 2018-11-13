8344d1e438cc5485e5d64a06eea75431bf10cd2f
shikhar <shikhar.patel@capgemini.com>
2018-11-13 17:14:45 +0530

    file added

---
+++ MProject/src/main/java/com/example/MavenProject/MavenProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.MavenProject;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenProjectApplication {
+
+	public static void main(String[] args) {
+	
+		System.out.println("hello world");
+	}
+}
